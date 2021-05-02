af3552be3891c3ac2260267ca8b143fc49d8b712
Artur Tagisow <artur.tagisow@billennium.com>
2021-05-02 18:03:11 +0200

    working with hardcoded vpc

--- deploy-stack.cf.sh
+++ deploy-stack.cf.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-STACK_NAME=goodpartsofaws
-REGION=eu-central-1
 CLI_PROFILE=default
+STACK_NAME=rownolegle
 
-EC2_INSTANCE_TYPE=t2.micro #free
+REGION=eu-central-1
+EC2_INSTANCE_TYPE=t2.nano #cheapest i think
 
-echo -e "\n\n==Deploying stack.cf.yaml===\n"
+#go to https://eu-central-1.console.aws.amazon.com/vpc/home?region=eu-central-1#vpcs:
+VPC_ID=vpc-74f9131e
+
+#go to https://eu-central-1.console.aws.amazon.com/vpc/home?region=eu-central-1#subnets:
+SUBNET1=subnet-8637e5ec
+SUBNET2=subnet-f8aeb2b5
+OPERATOR_EMAIL=atagisow@protonmail.com
+KEY_NAME=rownolegle
+
+echo -e "\n==Deploying stack.cf.yaml===\n"
 aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
   --stack-name $STACK_NAME \
-  --template-file stack.cf.yaml \
+  --template-file stack.cf.json \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-    EC2InstanceType=$EC2_INSTANCE_TYPE
-
+    VpcId=$VPC_ID \
+    Subnets="$SUBNET1,$SUBNET2" \
+    InstanceType=$EC2_INSTANCE_TYPE \
+    OperatorEMail=$OPERATOR_EMAIL \
+    KeyName=$KEY_NAME \
 
 if [$? -eq 0]; then
   aws cloudformation list-exports \
